8555699ca4f095fec52f3d3127576b3dcd5f9ae6
KorzunAV <KorzunAV@gmail.com>
2018-06-30 11:53:23 +0300

    extend Sha256Manager update nuget

--- Sources/Cryptography.ECDSA/Sha256Manager.cs
+++ Sources/Cryptography.ECDSA/Sha256Manager.cs
@@ -5,11 +5,34 @@ namespace Cryptography.ECDSA
 {
     public class Sha256Manager
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="data"></param>
-        /// <returns></returns>
+        private readonly Sha256T _sha;
+
+        public Sha256Manager()
+        {
+            _sha = new Sha256T();
+            Hash.Initialize(_sha);
+        }
+
+
+        public void Write(byte[] data)
+        {
+            Hash.Write(_sha, data, (UInt32)data.Length);
+        }
+
+        public void Write(byte[] data, int len)
+        {
+            Hash.Write(_sha, data, (UInt32)len);
+        }
+
+        public byte[] FinalizeAndGetResult()
+        {
+            byte[] outputSer = new byte[32];
+            Hash.Finalize(_sha, outputSer);
+            return outputSer;
+        }
+
+
+        
         public static byte[] GetHash(byte[] data)
         {
             Sha256T sha = new Sha256T();
